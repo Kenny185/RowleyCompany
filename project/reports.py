03f9c3a6032dd881840cba8de14724e5fc3ea128
Kennedy Wambua <kenedywambuah@gmail.com>
2024-04-10 17:41:29 +0300

    Styled the booking history page

--- project/reports.py
+++ project/reports.py
@@ -2,9 +2,12 @@ from io import BytesIO
 from flask import Blueprint, make_response, jsonify
 from flask_login import login_required
 from reportlab.lib.pagesizes import letter
+from reportlab.lib import colors
 from reportlab.pdfgen import canvas
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
 from .models import Booking, Payment, User
-from . import db
+from reportlab.platypus.tables import Table
 
 reports = Blueprint('reports', __name__)
 
@@ -25,7 +28,7 @@ def booking_report():
     bookings = Booking.query.all()
     bookings_data = [{ 'Username': booking.username, 'Email': booking.email, 
                       'Telephone': booking.telephone,  'Property_type': booking.property_type, 
-                      'Booking_hours': booking.booking_hours, 'Date': booking.date
+                      'Booking_hours': booking.booking_hours, 'Date': booking.date.strftime('%d/%m/%Y')
     }for booking in bookings]
     return jsonify(bookings_data)    
       
@@ -51,43 +54,51 @@ def generate_pdf(report_type):
     data, headers = report_data_functions[report_type]
     
     buffer = BytesIO()
-    pdf = canvas.Canvas(buffer, pagesize=letter)
-    def header(canvas, report_type):
-        canvas.saveState()
-        canvas.drawImage('/static/images/logo.jpeg', 50, 750, width=100, height=50)
-        canvas.setFont("Helvetica-Bold", 12)
-        canvas.drawString(100, 750, f"{report_type.capitalize()} Report")
-        canvas.restoreState()
+    pdf = SimpleDocTemplate(buffer, pagesize=letter)
+    elements = []
+
+    logo_path = 'project/static/images/de30038bf36a1146441892552da72a4e.jpg'
+    logo = Image(logo_path, width=50, height=50)
+    rowley_company_text = Paragraph("<b>Rowley Company</b>", ParagraphStyle(name='RowleyTextStyle', fontSize=15, alignment=1))
+    logo_and_text = [[logo, Spacer(0, 1), rowley_company_text]]
+    logo_and_text_table = Table(logo_and_text, colWidths=[100, None, None], style=[('ALIGN', (0, 0), (-1, -1), 'CENTRE')])
+
+    elements.append(logo_and_text_table)
         
-    pdf._header = lambda canvas, report_type: header(canvas, report_type)
+        
+    title_style = getSampleStyleSheet()["Title"]
+    title_text = f"{report_type.capitalize()} Report"
+    elements.append(Spacer(1, 20)) 
+    elements.append(Paragraph(title_text, title_style))
+
+    elements.append(Spacer(1, 20)) 
     
-    row_height = 720
-    for header in headers:
-        pdf.drawString(headers.index(header) * 200 + 50, row_height, header)
-    row_height -= 20
+    table_heading_style = [('BACKGROUND', (0, 0), (-1, 0), colors.gray),
+                           ('TEXTCOLOR', (0, 0), (-1, 0), colors.white),
+                           ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+                           ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+                           ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
+                           ('LEFTPADDING', (0, 0), (-1, -1), 10),
+                           ('RIGHTPADDING', (0, 0), (-1, -1), 10), 
+                           ('TOPPADDING', (0, 0), (-1, -1), 10), 
+                           ('BOTTOMPADDING', (0, 0), (-1, -1), 10)]
 
+    # Create style for table rows
+    table_row_style = [('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+                       ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
+                       ('LEFTPADDING', (0, 0), (-1, -1), 10),
+                       ('RIGHTPADDING', (0, 0), (-1, -1), 10),
+                       ('TOPPADDING', (0, 0), (-1, -1), 10), 
+                       ('BOTTOMPADDING', (0, 0), (-1, -1), 10)]
+    data_rows = []
     for item in data():
-        if report_type == 'clients':
-            pdf.drawString(50, row_height, item.surname)
-            pdf.drawString(250, row_height, item.first_name)
-            pdf.drawString(450, row_height, item.second_name)
-            pdf.drawString(650, row_height, item.telephone)
-            pdf.drawString(850, row_height, item.email)
-        elif report_type == 'bookings':
-            pdf.drawString(50, row_height, item.user.username)
-            pdf.drawString(250, row_height, item.service.email)
-            pdf.drawString(450, row_height, item.service.telephone)
-            pdf.drawString(650, row_height, item.service.property_type)
-            pdf.drawString(850, row_height, item.service.booking_hours)
-            pdf.drawString(1050, row_height, item.service.date)
-        elif report_type == 'payments':
-            pdf.drawString(50, row_height, item.full_name)
-            pdf.drawString(250, row_height, item.id_number)
-            pdf.drawString(450, row_height, item.service_type)
-            pdf.drawString(650, row_height, item.mpesa_number)
-        row_height -= 20
+        data_rows.append([str(getattr(item, header.lower(), '')) for header in headers])
+        
+    table_data = [headers] + data_rows
+    table = Table(table_data, style=table_heading_style + table_row_style, hAlign='CENTRE')
+    elements.append(table)
 
-    pdf.save()
+    pdf.build(elements)
     buffer.seek(0)
 
     response = make_response(buffer.getvalue())
